c29f3e1fb53f179d57ccd5e6fee6afd1c56ff814
brenhg15 <brenhg@hotmail.com>
2026-07-07 16:36:35 +0200

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -17,13 +17,14 @@
 # include <unistd.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
+#  define BUFFER_SIZE
 # endif
 
 char	*get_next_line(int fd);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s);
-char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlen(const char *s);
+void	*ft_calloc(size_t nmemb, size_t size);
 
 #endif
